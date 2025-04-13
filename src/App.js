8359be46df4fb6cd1fded603b3f490c0b5992231
Ishan Nara <naraishan924@gmail.com>
2025-04-13 14:26:00 +0530

    website restourant

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 // import ReactTable from "./component/ReactTable";
 // import AutoComplete from "./component/AutoComplete";
-import GridSystem from "./component/GridSystem";
+// import GridSystem from "./component/GridSystem";
 // import ImagesSelecter from "./components/ImagesSelecter";
 // import ImageSelect from "./components/ImageSelect";
 // import Login from "./Login";
@@ -10,6 +12,12 @@ import GridSystem from "./component/GridSystem";
 // import ColorPicker from "./ImportantReactHooks/ColorPicker";
 // import AlertBtnWebPage from "./alertBtnOnLocalHost/AlertBtnWebPage";
 // import UseRef from "./ImportantReactHooks/UseRef";
+
+import Home from "./RstourantWebsite/pages/Home"
+import About from "./RstourantWebsite/pages/About"
+import Contact from "./RstourantWebsite/pages/Contact"
+import Menu from "./RstourantWebsite/pages/Menu"
+import Pagenotfound from "./RstourantWebsite/pages/Pagenotfound"
 
 function App() {
   return (
@@ -22,10 +30,20 @@ function App() {
         {/* <ColorPicker/> */}
         {/* <ImageSelect/> */}
         {/* <ImagesSelecter/> */}
-        <GridSystem/>
+        {/* <GridSystem/> */}
         {/* <AutoComplete/> */}
         {/* <ReactTable/> */}
       </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="*" element={<Pagenotfound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
